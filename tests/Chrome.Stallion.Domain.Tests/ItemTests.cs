879310bd556b96d7fb1176555b0492f777a8790a
John Browne <brownej1331@gmail.com>
2022-04-19 16:18:53 -0400

    Lab 5.4, Item Tests

---
+++ tests/Chrome.Stallion.Domain.Tests/ItemTests.cs
@@ -0,0 +1,38 @@
+using Chrome.Stallion.Domain.Catalog;
+
+//had to add this because throwing error of "The type or namespace name 'ArgumentException' could not be found"
+using System;
+
+namespace Chrome.Stallion.Domain.Tests
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Item()
+        {
+            var item = new Item("Name", "Description", "Brand", 10.00m);
+
+            Assert.AreEqual("Name", item.Name);
+            Assert.AreEqual("Description", item.Description);
+            Assert.AreEqual("Brand", item.Brand);
+            Assert.AreEqual(10.00m, item.Price);
+
+        }
+        
+        [TestMethod]
+        public void Can_Create_New_Rating()
+        {
+            // Arrange
+            var item = new Item("Name", "Description", "Brand", 10.00m);
+            var rating = new Rating(5, "Name", "Review");
+
+            // Act
+            item.AddRating(rating);
+
+            // Assert
+            Assert.AreEqual(rating, item.Ratings[0]);
+        }
+
+    }
+}
